a3925d85f21eee6224af28b3e4ffc209bcf38fe4
Karl Ruudi <karl.ruudi@ttu.ee>
2016-02-25 10:43:53 +0200

    ae

---
+++ sisselogimine/js/sisselogimine.js
@@ -0,0 +1,41 @@
+function resetForm(form) {
+    // clearing inputs
+    var inputs = form.getElementsByTagName('input');
+    for (var i = 0; i<inputs.length; i++) {
+        switch (inputs[i].type) {
+            // case 'hidden':
+            case 'text':
+                inputs[i].value = '';
+                break;
+            case 'radio':
+            case 'checkbox':
+                inputs[i].checked = false;   
+        }
+    }
+
+    // clearing selects
+    var selects = form.getElementsByTagName('select');
+    for (var i = 0; i<selects.length; i++)
+        selects[i].selectedIndex = 0;
+
+    // clearing textarea
+    var text= form.getElementsByTagName('textarea');
+    for (var i = 0; i<text.length; i++)
+        text[i].innerHTML= '';
+
+    return false;
+}
+    
+function validateForm(form) {
+    var un = form.1.value + form.2.value + form.3.value + form.4.value + form.5.value;
+    var pw = form.6.value + form.7.value + form.8.value + form.9.value + form.10.value;
+    var username = "asd@asd.ee"; 
+    var password = "asd";
+    if ((un == username) && (pw == password)) {
+        return true;
+    }
+    else {
+        alert ("Login was unsuccessful, please check your username and password");
+        return false;
+    }
+}
