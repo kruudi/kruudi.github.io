e2aed81315d664cd603d62b1b97ac4f0c3191563
kruudi <kruudi@users.noreply.github.com>
2022-03-13 14:48:59 +0200

    additions

--- Valle/js/projectload.js
+++ Valle/js/projectload.js
@@ -15,28 +15,50 @@
 
     		return this.each(function() {
 				var o = options;
-
+                console.log(o.elementcnt);
+                
                  $.getJSON('assets/pageContents.json', function(data) {
-                    $.each(data.project, function(i, f) {
-                        var projectElement = '<div class="row justify-content-center no-gutters mb-5 mb-lg-0">' +
-                            '<div class="col-lg-6"><img class="img-fluid float-right" src="assets/img/' + f.asset + '" alt="" /></div>' +
-                            '<div class="col-lg-6">' +
-                            '<div class="bg-black text-center h-100 project">' +
-                            '<div class="d-flex h-100">' +
-                            '<div class="project-text w-100 my-auto text-center text-lg-left">' +
-                            '<h4 class="text-white">' + f.title +
-                            '</h4>' +
-                            '<p class="mb-0 text-white-50">' + f.content +
-                            '</p>' +
-                            '<hr class="d-none d-lg-block mb-0 ml-0" />' +
-                            '</div>' +
-                            '</div>' +
-                            '</div>' +
-                            '</div>' +
-                            '</div>'
-                        console.log(1);
-                        $(projectElement).appendTo(this);
-                    });
+                     console.log("json loaded")
+                     $.each(data.projects, function(i, e) {
+                        if (o.elementcnt >= e.projectno) {
+                            if (e.projectno % 2 === 0) {
+                                var projectElement = '<div class="row justify-content-center no-gutters mb-5 mb-lg-0">' +
+                                '<div class="col-lg-6"><img class="img-fluid float-right" src="assets/img/' + e.asset + '" alt="" /></div>' +
+                                '<div class="col-lg-6">' +
+                                '<div class="bg-black text-center h-100 project">' +
+                                '<div class="d-flex h-100">' +
+                                '<div class="project-text w-100 my-auto text-center text-lg-left">' +
+                                '<h4 class="text-white">' + e.title +
+                                '</h4>' +
+                                '<p class="mb-0 text-white-50">' + e.content +
+                                '</p>' +
+                                '<hr class="d-none d-lg-block mb-0 ml-0" />' +
+                                '</div>' +
+                                '</div>' +
+                                '</div>' +
+                                '</div>' +
+                                '</div>'
+                            } else {
+                                 var projectElement = '<div class="row justify-content-center no-gutters">' +
+                                '<div class="col-lg-6"><img class="img-fluid float-left" src="assets/img/' + e.asset + '" alt="" /></div>' +
+                                '<div class="col-lg-6 order-lg-first">' +
+                                '<div class="bg-black text-center h-100 project">' +
+                                '<div class="d-flex h-100">' +
+                                '<div class="project-text w-100 my-auto text-center text-lg-right">' +
+                                '<h4 class="text-white">' + e.title +
+                                '</h4>' +
+                                '<p class="mb-0 text-white-50">' + e.content +
+                                '</p>' +
+                                '<hr class="d-none d-lg-block mb-0 ml-0" />' +
+                                '</div>' +
+                                '</div>' +
+                                '</div>' +
+                                '</div>' +
+                                '</div>'
+                            }
+                            $(projectElement).appendTo("#projectlist");
+                        }
+                     });
                 });
 			
     		});
