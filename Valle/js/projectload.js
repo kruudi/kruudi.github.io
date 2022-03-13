9ccf128252f3461601e59a946d2c387bb29c90cb
kruudi <kruudi@users.noreply.github.com>
2022-03-13 13:53:58 +0200

    Update projectload.js

--- Valle/js/projectload.js
+++ Valle/js/projectload.js
@@ -1,24 +1,46 @@
-(function ($) {
-    $.getJSON('assets/pageContents.json', function(data) {
-        $.each(data.project, function(i, f) {
-            var projectElement = '<div class="row justify-content-center no-gutters mb-5 mb-lg-0">' +
-                '<div class="col-lg-6"><img class="img-fluid float-right" src="assets/img/' + f.asset + '" alt="" /></div>' +
-                '<div class="col-lg-6">' +
-                '<div class="bg-black text-center h-100 project">' +
-                '<div class="d-flex h-100">' +
-                '<div class="project-text w-100 my-auto text-center text-lg-left">' +
-                '<h4 class="text-white">' + f.title +
-                '</h4>' +
-                '<p class="mb-0 text-white-50">' + f.content +
-                '</p>' +
-                '<hr class="d-none d-lg-block mb-0 ml-0" />' +
-                '</div>' +
-                '</div>' +
-                '</div>' +
-                '</div>' +
-                '</div>'
-            console.log(projectElement);
-            $(projectElement).appendTo("#concerts div");
-        });
-    });
+(function($){
+
+ 	$.fn.extend({ 
+ 		
+		//pass the options variable to the function
+ 		projectload: function(options) {
+
+
+			//Set the default values, use comma to separate the settings, example:
+			var defaults = {
+				elementcnt: 2
+			}
+				
+			var options =  $.extend(defaults, options);
+
+    		return this.each(function() {
+				var o = options;
+
+                 $.getJSON('assets/pageContents.json', function(data) {
+                    $.each(data.project, function(i, f) {
+                        var projectElement = '<div class="row justify-content-center no-gutters mb-5 mb-lg-0">' +
+                            '<div class="col-lg-6"><img class="img-fluid float-right" src="assets/img/' + f.asset + '" alt="" /></div>' +
+                            '<div class="col-lg-6">' +
+                            '<div class="bg-black text-center h-100 project">' +
+                            '<div class="d-flex h-100">' +
+                            '<div class="project-text w-100 my-auto text-center text-lg-left">' +
+                            '<h4 class="text-white">' + f.title +
+                            '</h4>' +
+                            '<p class="mb-0 text-white-50">' + f.content +
+                            '</p>' +
+                            '<hr class="d-none d-lg-block mb-0 ml-0" />' +
+                            '</div>' +
+                            '</div>' +
+                            '</div>' +
+                            '</div>' +
+                            '</div>'
+                        console.log(projectElement);
+                        $(projectElement).appendTo("#concerts div");
+                    });
+                });
+			
+    		});
+    	}
+	});
+	
 })(jQuery);
